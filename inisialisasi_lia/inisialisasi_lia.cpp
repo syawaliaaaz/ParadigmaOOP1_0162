43ec9cc5e40d4ce5d8a378a8bca720bc4e1d60f7
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-21 10:31:46 +0700

    Add project files.

---
+++ inisialisasi_lia/inisialisasi_lia.cpp
@@ -0,0 +1,14 @@
+// inisialisasi_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
