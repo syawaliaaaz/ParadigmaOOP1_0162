08f28b9b13b186f8d078e8620a057141095921dc
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-21 10:35:11 +0700

    Membuat class mahasiswa, private dan public

--- inisialisasi_lia/inisialisasi_lia.cpp
+++ inisialisasi_lia/inisialisasi_lia.cpp
@@ -5,7 +5,17 @@
 #include <string>
 using namespace std;
 
-class mahasiswa {};
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+        //Definisi
+    }
+};
 
 int main()
 {
