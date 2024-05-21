fcfc3b395c9e80af2750cfe29dbb56181e276fc3
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-21 17:51:01 +0700

    Deklarasi Integer main sampai return 0

--- agregasi_lia/agregasi_lia.cpp
+++ agregasi_lia/agregasi_lia.cpp
@@ -8,8 +8,25 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);   
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+
+    return 0;
 }
+
 
