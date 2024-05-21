8aeb5c7a1dc120c157f3e7b4aab725ca5de115d5
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-21 10:53:42 +0700

    Membuat header dari jantung.h

---
+++ komposisi_lia/jantung.h
@@ -0,0 +1,13 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
---
+++ komposisi_lia/komposisi_lia.cpp
@@ -0,0 +1,13 @@
+// komposisi_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "jantung.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
