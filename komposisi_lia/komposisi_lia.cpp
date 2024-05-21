53b6748858a568df29ca5f2044aea23a97378070
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-21 11:04:54 +0700

    Deklarasi Integer Main sampai return 0

--- komposisi_lia/komposisi_lia.cpp
+++ komposisi_lia/komposisi_lia.cpp
@@ -8,7 +8,9 @@ using namespace std;
 #include "jantung.h"
 #include "manusia.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    manusia* varManusia = new manusia("jono");
+    delete varManusia;
+    return 0;
+    
 }
